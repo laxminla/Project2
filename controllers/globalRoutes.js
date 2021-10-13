9a1d961c6f679d9706caa4ac4f4b07d16c1bb7fc
Laxmi Sharma <laxminla@yahoo.com>
2021-10-13 09:56:14 -0700

    added public and view folder

--- controllers/globalRoutes.js
+++ controllers/globalRoutes.js
@@ -2,7 +2,16 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     // Send the rendered Handlebars.js template back as the response
-    res.render('home');
+    res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/login');
 });
 
 module.exports = router;
