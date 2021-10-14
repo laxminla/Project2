cf38753a9d5d7fcd1879e66b915004e35ef2bec6
Laxmi Sharma <laxminla@yahoo.com>
2021-10-13 17:16:36 -0700

    modified routes and controllers

--- controllers/api/consumerRoutes.js
+++ controllers/api/consumerRoutes.js
@@ -1,83 +1,80 @@
 const router = require('express').Router();
-const { Consumer, Producer, Items } = require('../../models');
+const { Consumer, Producer, Items } = require('../../../../groupproject-2/Project2Final/Project2/models');
 
 
 // Get all consumer
 router.get('/', async (req, res) => {
-    try {
-      const consumerData = await Consumer.findAll(req.body, {
-        include: [Items]  
-      });
-      res.status(200).json(consumerData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const consumerData = await Consumer.findAll(req.body, {
+      include: [Items]
+    });
+    res.status(200).json(consumerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //   Find single consumer
-  router.get('/:id', async (req, res) => {
-   
-    try {
-      const consumerData = await Consumer.findByPk(req.params.id);
-      if (!consumerData) {
-        res.status(404).json({ message: "No consumer with this id" });
-      }
-      res.status(200).json(consumerData);
-    } catch (err) {
-      res.status(500).json(err);
-      console.log(err);
+router.get('/:id', async (req, res) => {
+
+  try {
+    const consumerData = await Consumer.findByPk(req.params.id);
+    if (!consumerData) {
+      res.status(404).json({ message: "No consumer with this id" });
     }
-    const consumerData = consumerData.get({ plain: true });
-    res.render('consumer', consumer);
-  });
+    res.status(200).json(consumerData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+  const consumerData = consumerData.get({ plain: true });
+  res.render('consumer', consumer);
+});
 
 
-  // create a new consumer
-  router.post('/', (req, res) => {
-    Consumer.create({
-      consumer_name: req.body.consumer_name
+// create a new consumer
+router.post('/', (req, res) => {
+  Consumer.create(req.body)
+    .then(dbConsumerData => res.json(dbConsumerData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     })
-      .then(dbConsumerData => res.json(dbConsumerData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      })
-  
-  });
+
+});
 
 //   update a consumer by its 'id' value
 router.put('/:id', (req, res) => {
-    Consumer.update(req.body, {
-      where: {
-        id: req.params.id
+  Consumer.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbConsumerData => {
+      if (!dbConsumerData[0]) {
+        res.status(404).json({ message: "No consumer found with this id" });
+        return;
       }
+      res.json(dbConsumerData);
     })
-      .then(dbConsumerData => {
-        if (!dbConsumerData[0]) {
-          res.status(404).json({ message: "No consumer found with this id" });
-          return;
-        }
-        res.json(dbConsumerData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-
-  // delete a consumer by its `id` value
-  router.delete('/:id', (req, res) => {
-    const deletedConsumer = Consumer.destroy({
-      where: {
-        id: req.params.id,
-      }
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    res.json(deletedConsumer);
+});
+
+
+// delete a consumer by its `id` value
+router.delete('/:id', (req, res) => {
+  const deletedConsumer = Consumer.destroy({
+    where: {
+      id: req.params.id,
+    }
   });
+  res.json(deletedConsumer);
+});
 
-  module.exports = router;
+module.exports = router;
 
 
- 
-  
+
